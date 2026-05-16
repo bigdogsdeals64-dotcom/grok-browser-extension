eb553595fdd5f817dc8cee0fcae279ba56c02419
Zack R Brown <bigdogsdeals64@gmail.com>
2026-05-16 11:16:39 -0400

    Add background.js

---
+++ background.js
@@ -0,0 +1,11 @@
+chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete' && tab.url) {
+    chrome.sidePanel.setOptions({
+      tabId,
+      path: 'sidebar.html',
+      enabled: true
+    });
+  }
+});
